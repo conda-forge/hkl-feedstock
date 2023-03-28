6e13af5cefe0fc619545ec5f66acca4ab9a8192c
Pete R Jemian <prjemian@users.noreply.github.com>
2023-03-28 16:05:59 -0500

    MNT #13 configure HDF5

--- recipe/build.sh
+++ recipe/build.sh
@@ -8,14 +8,15 @@ test -d m4 || mkdir m4
 export ACLOCAL_PATH="$PREFIX/share/aclocal"
 aclocal --print-ac-dir
 
+export HDF5_CFLAGS=" -I${CONDA_PREFIX}/include"
+export HDF5_LIBS=" -L${CONDA_PREFIX}/lib -lhdf5"
+
 autoreconf -ivf
 
 ./configure \
     --disable-static \
     --disable-gui \
     --enable-introspection=yes \
-    --disable-gtk-doc \
-    --disable-hkl-doc \
     --prefix=$PREFIX
 make -j ${CPU_COUNT}
 make install
