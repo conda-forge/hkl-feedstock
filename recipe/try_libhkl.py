e6568acf0735e089d3f96e625f4763d0e87aba61
Pete R Jemian <prjemian@users.noreply.github.com>
2023-10-02 17:12:15 -0500

    TST Which test is failing?

--- recipe/try_libhkl.py
+++ recipe/try_libhkl.py
@@ -5,17 +5,29 @@ gi.require_version("Hkl", "5.0")
 from gi.repository import Hkl as libhkl
 
 # access some content
-diffractometer_types = libhkl.factories()
-assert isinstance(diffractometer_types, dict), f"{diffractometer_types=}"
-assert 5 < len(diffractometer_types) < 30, f"{diffractometer_types=}"
-for i, dt in enumerate(sorted(diffractometer_types), start=1):
-    print(f"{i} {dt}  {diffractometer_types[dt]}")
+try:
+    diffractometer_types = libhkl.factories()
+    assert isinstance(diffractometer_types, dict), f"{diffractometer_types=}"
+    assert 5 < len(diffractometer_types) < 30, f"{diffractometer_types=}"
+    for i, dt in enumerate(sorted(diffractometer_types), start=1):
+        print(f"{i} {dt}  {diffractometer_types[dt]}")
+except AssertionError as exc:
+    print(f"{exc=}")
 
-UserUnits = libhkl.UnitEnum.USER
-assert UserUnits is not None, f"{dir(libhkl.UnitEnum)=}"
+try:
+    UserUnits = libhkl.UnitEnum.USER
+    assert UserUnits is not None, f"{dir(libhkl.UnitEnum)=}"
+except AssertionError as exc:
+    print(f"{exc=}")
 
-DefaultUnits = libhkl.UnitEnum.DEFAULT
-assert DefaultUnits is not None, f"{dir(libhkl.UnitEnum)=}"
+try:
+    DefaultUnits = libhkl.UnitEnum.DEFAULT
+    assert DefaultUnits is not None, f"{dir(libhkl.UnitEnum)=}"
+except AssertionError as exc:
+    print(f"{exc=}")
 
-assert "VERSION" in dir(libhkl), f"{dir(libhkl)=}"
-assert libhkl.VERSION.startswith("v5.0.0"), f"{libhkl.VERSION=}"
+try:
+    assert "VERSION" in dir(libhkl), f"{dir(libhkl)=}"
+    assert libhkl.VERSION.startswith("v5.0.0"), f"{libhkl.VERSION=}"
+except AssertionError as exc:
+    print(f"{exc=}")
