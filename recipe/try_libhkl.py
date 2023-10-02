5ff9a77c787ff02166018eb0d9b32e57dc91bc60
Pete R Jemian <prjemian@users.noreply.github.com>
2023-10-02 16:51:29 -0500

    add more output

--- recipe/try_libhkl.py
+++ recipe/try_libhkl.py
@@ -6,16 +6,16 @@ from gi.repository import Hkl as libhkl
 
 # access some content
 diffractometer_types = libhkl.factories()
-assert isinstance(diffractometer_types, dict)
-assert 5 < len(diffractometer_types) < 30
+assert isinstance(diffractometer_types, dict), f"{diffractometer_types=}"
+assert 5 < len(diffractometer_types) < 30, f"{diffractometer_types=}"
 for i, dt in enumerate(sorted(diffractometer_types), start=1):
     print(f"{i} {dt}  {diffractometer_types[dt]}")
 
 UserUnits = libhkl.UnitEnum.USER
-assert UserUnits is not None
+assert UserUnits is not None, f"{dir(libhkl.UnitEnum)=}"
 
 DefaultUnits = libhkl.UnitEnum.DEFAULT
-assert DefaultUnits is not None
+assert DefaultUnits is not None, f"{dir(libhkl.UnitEnum)=}"
 
-assert "VERSION" in dir(libhkl)
-assert libhkl.VERSION.startswith("v5.0.0")
+assert "VERSION" in dir(libhkl), f"{dir(libhkl)=}"
+assert libhkl.VERSION.startswith("v5.0.0"), f"{libhkl.VERSION=}"
