918eb2f1ef2b28d5c5e513f717531d750b12e5b5
Pete R Jemian <prjemian@users.noreply.github.com>
2023-10-02 15:02:41 -0500

    Update recipe/build.sh
    Co-authored-by: Max Rakitin <mrakitin@users.noreply.github.com>

--- recipe/build.sh
+++ recipe/build.sh
@@ -10,7 +10,7 @@ ls -lAFgh
 
 echo "DIAGNOSTIC pwd: $(pwd)"
 
-TARBALL="libhkl-${PKG_VERSION}-x86_64.tar.gz"
+TARBALL="libhkl-v${PKG_VERSION}-x86_64.tar.gz"
 echo "Installing from ${TARBALL}"
 tar xzf "${TARBALL}" -C "${PREFIX}"
 echo "DIAGNOSTIC installed directory:"
