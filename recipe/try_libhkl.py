cc63b80af933fa44d4dee56707167240e59ba3cd
Pete R Jemian <prjemian@users.noreply.github.com>
2023-10-03 09:56:13 -0500

    TST revert test code to full

--- recipe/try_libhkl.py
+++ recipe/try_libhkl.py
@@ -1,29 +1,26 @@
 """Test that libhkl pre-built support is available in Python."""
 
-try:
-    # import gi  # gobject-introspection, to access libhkl
-    import sys
-except Exception as exc:
-    print(f"{exc=}")
-# assert "require_version" in dir(gi)
+import gi  # gobject-introspection, to access libhkl
+assert "require_version" in dir(gi)
 
-# gi.require_version("Hkl", "5.0")
-# from gi.repository import Hkl as libhkl
+gi.require_version("Hkl", "5.0")
+assert "Hkl" in dir(gi.repository)
 
-# # access some content
-# diffractometer_types = libhkl.factories()
-# assert isinstance(diffractometer_types, dict), f"{diffractometer_types=}"
-# assert 5 < len(diffractometer_types) < 30, f"{diffractometer_types=}"
-# for i, dt in enumerate(sorted(diffractometer_types), start=1):
-#     print(f"{i} {dt}  {diffractometer_types[dt]}")
+from gi.repository import Hkl as libhkl
+assert libhkl is not None
 
-# UserUnits = libhkl.UnitEnum.USER
-# assert UserUnits is not None, f"{dir(libhkl.UnitEnum)=}"
+# access some content
+diffractometer_types = libhkl.factories()
+assert isinstance(diffractometer_types, dict), f"{diffractometer_types=}"
+assert 5 < len(diffractometer_types) < 30, f"{diffractometer_types=}"
+for i, dt in enumerate(sorted(diffractometer_types), start=1):
+    print(f"{i} {dt}  {diffractometer_types[dt]}")
 
-# DefaultUnits = libhkl.UnitEnum.DEFAULT
-# assert DefaultUnits is not None, f"{dir(libhkl.UnitEnum)=}"
+UserUnits = libhkl.UnitEnum.USER
+assert UserUnits is not None, f"{dir(libhkl.UnitEnum)=}"
 
-# assert "VERSION" in dir(libhkl), f"{dir(libhkl)=}"
-# assert libhkl.VERSION.startswith("v5.0.0"), f"{libhkl.VERSION=}"
+DefaultUnits = libhkl.UnitEnum.DEFAULT
+assert DefaultUnits is not None, f"{dir(libhkl.UnitEnum)=}"
 
-assert True
+assert "VERSION" in dir(libhkl), f"{dir(libhkl)=}"
+assert libhkl.VERSION.startswith("v5.0.0"), f"{libhkl.VERSION=}"
