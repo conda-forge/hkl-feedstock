972354e2fbd771643353b55d88fbfe8b4c1c49a2
Pete R Jemian <prjemian@users.noreply.github.com>
2024-07-27 18:37:01 -0500

    TST refactor TEST_MODES

--- recipe/try_libhkl.py
+++ recipe/try_libhkl.py
@@ -24,14 +24,6 @@ assert DefaultUnits is not None
 TEST_GEOMETRY = "E4CV"
 TEST_AXIS_LIST = "omega chi phi tth".split()
 TEST_ENGINE = "hkl"
-TEST_MODES = """
-    bissector
-    constant_omega
-    constant_chi
-    constant_phi
-    double_diffraction
-    psi_constant
-""".strip().splitlines()
 
 factory = diffractometer_types[TEST_GEOMETRY]
 engine_list = factory.create_new_engine_list()
@@ -39,7 +31,13 @@ engine_names = [e.name_get() for e in engine_list.engines_get()]
 assert TEST_ENGINE in engine_names
 
 engine = engine_list.engine_get_by_name(TEST_ENGINE)
-assert engine.modes_names_get() == TEST_MODES, f"{engine.modes_names_get()=}"
+modes = engine.modes_names_get() 
+assert "bissector" in modes, f"{modes=}"
+assert "constant_omega" in modes, f"{modes=}"
+assert "constant_chi" in modes, f"{modes=}"
+assert "constant_phi" in modes, f"{modes=}"
+assert "double_diffraction" in modes, f"{modes=}"
+assert "psi_constant" in modes, f"{modes=}"
 
 geometry = factory.create_new_geometry()
 assert geometry.axis_names_get() == TEST_AXIS_LIST
