c2e7461b7308fd4ab08a5ce2558e19961a2c1ff1
Pete R Jemian <prjemian@users.noreply.github.com>
2025-11-20 13:02:12 -0600

    Support portable relative path

--- recipe/build.sh
+++ recipe/build.sh
@@ -21,6 +21,18 @@ grep AM_DISTCHECK_CONFIGURE_FLAGS Makefile.am
 printf "### ---> (%s) Running autogen.sh ... \n" $(date -Iseconds)
 bash ./autogen.sh
 
+# Use $ORIGIN so the installed package is relocatable by conda.
+# Quote/escape $ORIGIN so the shell does not expand it here.
+ORIGIN_RPATH='\$ORIGIN/../lib'
+# If your package installs into lib64, change to '\$ORIGIN/../lib64'
+RPATH_FLAG="-Wl,-rpath,${ORIGIN_RPATH}"
+
+# Export flags used by autotools configure
+export LDFLAGS="${LDFLAGS:-} ${RPATH_FLAG}"
+export CPPFLAGS="${CPPFLAGS:-} -I${PREFIX}/include"
+export CFLAGS="${CFLAGS:-}"
+export PKG_CONFIG_PATH="${PKG_CONFIG_PATH}:${BUILD_PREFIX}/lib/pkgconfig"
+
 printf "### ---> (%s) Running configure ... \n" $(date -Iseconds)
 ./configure \
   --prefix="${PREFIX}" \
@@ -28,7 +40,9 @@ printf "### ---> (%s) Running configure ... \n" $(date -Iseconds)
   --disable-binoculars \
   --disable-gui \
   --disable-hkl-doc \
-  --enable-introspection=yes
+  --enable-introspection=yes \
+  LDFLAGS="${LDFLAGS}"
+
 # printf "### ---> DIR : %s\n" $(ls)
 
 printf "### ---> (%s) Running make ... \n" $(date -Iseconds)
